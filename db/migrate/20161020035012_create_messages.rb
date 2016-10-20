36c79a23603e7e40957d0032f903268ea32435ca
lalalunnn <nozawa@nozawa-no-MacBook-Pro.local>
2016-10-20 13:32:28 +0900

    各テーブル：マイグレーション実行
    rake db:migrate実行。
    エラーが出たので下記記述を削除しました。
    foreign_key: true
    再$ rake db:migrate実行。

--- db/migrate/20161020035012_create_messages.rb
+++ db/migrate/20161020035012_create_messages.rb
@@ -3,8 +3,8 @@ class CreateMessages < ActiveRecord::Migration[5.0]
     create_table :messages do |t|
       t.text    :body, null: false
       t.string  :image
-      t.references :user, foreign_key: true
-      t.references :group, foreign_key: true
+      t.references :user
+      t.references :group
       t.timestamps
     end
   end
--- db/migrate/20161020040649_create_members.rb
+++ db/migrate/20161020040649_create_members.rb
@@ -1,8 +1,9 @@
 class CreateMembers < ActiveRecord::Migration[5.0]
   def change
     create_table :members do |t|
-      t.references :user, foreign_key: true
-      t.references :group, foreign_key: true
+      t.references :user
+      t.references :group
+
       t.timestamps
     end
   end
