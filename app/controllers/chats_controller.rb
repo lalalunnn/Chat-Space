42f02a8293e861f3adc22e31885e437a926ca078
lalalunnn <nozawa@nozawa-no-MacBook-Pro.local>
2016-10-28 12:08:18 +0900

    DBへ送信
    保存されるようにする

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,4 +1,16 @@
 class ChatsController < ApplicationController
+  
   def index
+    @messages = Message.all # messageモデル情報の全部を取得
+  end
+
+  def create
+    Message.create(message_params)
+    # redirect_to :action => "index"
+  end
+
+  private
+  def message_params
+    params.permit(:name, :image, :message[body])
   end
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,20 @@
 class MessagesController < ApplicationController
+
+  def index
+    @messages = Message.all
+    @message = Message.new
+  end
+
+  def create
+  
+     Message.create(create_params)
+     redirect_to action: :index
+  end
+
+  private
+    def create_params
+      # Message.create( image: params[:image], body: params[:body])
+      params.permit( :image, :body)
+    end
+
 end
