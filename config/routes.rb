f16c200e3c7598945976a014f16dca048d1c3f1c
lalalunnn <nozawa@nozawa-no-MacBook-Pro.local>
2016-11-04 17:42:33 +0900

    messagesコントローラーからgroupsコントローラーへindexを移動

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :groups, only: [:new , :create, :edit, :update] do
-    resources :messages,only: [:index, :new , :create]
+  resources :groups, only: [:index, :show, :new , :create, :edit, :update] do
+    resources :messages, only: [:new, :create]
   end
 
-  resources :users, only: [:edit , :update]
-  root  'messages#index'
+  resources :users, only: [:edit, :update]
+  root  'groups#index'
 
 end
