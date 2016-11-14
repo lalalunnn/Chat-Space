3ef9a028bf64985cc72e4d3e561ffdb5838150bd
lalalunnn <nozawa@nozawa-no-MacBook-Pro.local>
2016-11-14 13:41:45 +0900

    関数にしてまとめるapp/assets/javascripts/message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,3 +1,16 @@
+function build_html(data){
+  var addedMessage =
+    '<li class="chat__message">\
+      <div class="chat__message__header">\
+          <div class="chat__message__header--name">'+ data.nickname +'</div>\
+          <div class="chat__message__header--time">'+ data.datetime +'</div>\
+      </div>\
+      <div class="chat__message--body">'+ data.body +'</div>\
+    </li>';
+
+  $('.chat__messages').append(addedMessage);
+};
+
 $(function(){
   $('#submit').on('click', function(e) {
     var now_url = $(location).attr('href');
@@ -14,17 +27,7 @@ $(function(){
       data: {message: {body: message_body}}
 
     }).done(function(data){
-
-      var addedMessage =
-        '<li class="chat__message">\
-          <div class="chat__message__header">\
-              <div class="chat__message__header--name">'+ data.nickname +'</div>\
-              <div class="chat__message__header--time">'+ data.datetime +'</div>\
-          </div>\
-          <div class="chat__message--body">'+ data.body +'</div>\
-        </li>'
-
-      $('.chat__messages').append(addedMessage);
+      build_html(data);
 
     }).fail(function(xhr,status,error){
       console.log("通信失敗");
