73f98c0eafe350133b877aea62eb1da5d04c44e1
lalalunnn <nozawa@nozawa-no-MacBook-Pro.local>
2016-11-22 20:49:00 +0900

    書き方の変更のためバックアップ

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,8 +2,8 @@ function build_html(data){
   var addedMessage =
     '<li class="chat__message">\
       <div class="chat__message__header">\
-          <div class="chat__message__header--name">'+ data.nickname +'</div>\
-          <div class="chat__message__header--time">'+ data.datetime +'</div>\
+        <div class="chat__message__header--name">'+ data.nickname +'</div>\
+        <div class="chat__message__header--time">'+ data.datetime +'</div>\
       </div>\
       <div class="chat__message--body">'+ data.body +'</div>\
     </li>';
@@ -15,6 +15,8 @@ $(function(){
   $('#submit').on('click', function(e) {
     var now_url = $(location).attr('href');
     var message_body = $('#message_body').val();
+    // new_message
+
 
     $('#message_body').val('');
 
@@ -24,13 +26,15 @@ $(function(){
       url: now_url + "/messages.json",
       type: "POST",
       dataType: 'json',
-      data: {message: {body: message_body}}
+      // data: {message: {body: message_body}}
+      data: { formData }
 
     }).done(function(data){
       build_html(data);
+      console.log( 'SUCCESS', data );
 
     }).fail(function(xhr,status,error){
-      console.log("通信失敗");
+      console.log( 'ERROR', xhr,status,error );
 
     });
 
