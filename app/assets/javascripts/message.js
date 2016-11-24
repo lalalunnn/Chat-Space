6833a0f9033e355e3bd9aece2c4fab1732f7ceef
lalalunnn <nozawa@nozawa-no-MacBook-Pro.local>
2016-11-24 21:37:29 +0900

    非同期とビューに表示する

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,37 +1,67 @@
 function build_html(data){
   var addedMessage =
     '<li class="chat__message">\
-      <div class="chat__message__header">\
-        <div class="chat__message__header--name">'+ data.nickname +'</div>\
-        <div class="chat__message__header--time">'+ data.datetime +'</div>\
-      </div>\
-      <div class="chat__message--body">'+ data.body +'</div>\
+    <div class="chat__message__header">\
+    <div class="chat__message__header--name">'
+    + data.nickname +
+    '</div>\
+    <div class="chat__message__header--time">'
+    + data.datetime +
+    '</div>\
+    </div>\
+    <div class="chat__message--body">'
+    + data.body +
+    '</div>\
     </li>';
 
-  $('.chat__messages').append(addedMessage);
+  var addedMessage2 =
+    '<li class="chat__message">\
+    <div class="chat__message__header">\
+    <div class="chat__message__header--name">'
+    + data.nickname +
+    '</div>\
+    <div class="chat__message__header--time">'
+    + data.datetime +
+    '</div>\
+    </div>\
+    <div class="chat__message--body">'
+    + data.body +
+    '<img src='+data.image+'></div>\
+    </li>';
+
+      // $('.chat__messages').append(addedMessage2);
+    if (data.image != null) {
+      $('.chat__messages').append(addedMessage2);
+    } else{
+      $('.chat__messages').append(addedMessage);
+    };
 };
 
+
 $(function(){
-  $('#submit').on('click', function(e) {
+  $('#new_message').on('submit', function(e) {
+
+    var fd = new FormData($(this)[0]);
+
     var now_url = $(location).attr('href');
-    var message_body = $('#message_body').val();
-    // new_message
-
-
-    $('#message_body').val('');
+    var ajax_url = now_url + "/messages.json"
 
     e.preventDefault();
 
     $.ajax({
-      url: now_url + "/messages.json",
+      url: ajax_url,
       type: "POST",
       dataType: 'json',
-      // data: {message: {body: message_body}}
-      data: { formData }
+      data: fd,
+      processData: false,
+      contentType: false
 
     }).done(function(data){
       build_html(data);
-      console.log( 'SUCCESS', data );
+      console.log(data);
+
+      // formのデータをクリアする
+      $('#message_body').val('');
 
     }).fail(function(xhr,status,error){
       console.log( 'ERROR', xhr,status,error );
